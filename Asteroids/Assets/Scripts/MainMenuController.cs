052101be62b8b7b6525275842a185aa4dd0f002c
Dan Alexander <lxndrdagreat@gmail.com>
2017-03-03 22:14:43 -0500

    menu scene

---
+++ Asteroids/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	public void StartGame() {
+
+	}
+
+	public void QuitGame() {
+		// Note: this is ignored in the editor.
+		Application.Quit ();
+	}
+}
