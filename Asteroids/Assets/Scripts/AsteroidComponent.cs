3f8a1dd4c081c1fd5762b6e3333c8151299d3287
Dan Alexander <lxndrdagreat@gmail.com>
2017-03-04 23:44:33 -0500

    spawning asteroids and moving them around

---
+++ Asteroids/Assets/Scripts/AsteroidComponent.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidComponent : MonoBehaviour {
+
+	private Rigidbody2D m_RigidBody;
+
+	private Vector2 m_ForceDirection;
+
+	private SpriteRenderer m_Renderer;
+
+	private bool m_IsWrappingX = false;
+	private bool m_IsWrappingY = false;
+
+	void Awake() {
+		m_RigidBody = GetComponent<Rigidbody2D> ();
+
+		m_ForceDirection = new Vector2 (Random.Range (-1.0f, 1.0f), Random.Range (-1.0f, 1.0f));
+
+		m_RigidBody.AddTorque (Random.Range(-5.0f, 5.0f));
+
+		m_Renderer = GetComponent<SpriteRenderer> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_RigidBody.AddForce (m_ForceDirection);
+
+		var viewportPosition = Camera.main.WorldToViewportPoint (transform.position);
+
+		var newPosition = transform.position;
+			
+		if (viewportPosition.x > 1 || viewportPosition.x < 0) {
+			if (!m_IsWrappingX) {
+				newPosition.x = -newPosition.x;
+				m_IsWrappingX = true;
+			}
+		} else {
+			m_IsWrappingX = false;
+		}
+
+		if (viewportPosition.y > 1 || viewportPosition.y < 0) {
+			if (!m_IsWrappingY) {
+				newPosition.y = -newPosition.y;
+				m_IsWrappingY = true;
+			}
+		} else {
+			m_IsWrappingY = false;
+		}
+
+		transform.position = newPosition;
+	}
+}
---
+++ Asteroids/Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public static GameController instance;
+
+	private int m_Level = 1;
+	public int Level { get { return m_Level; } }
+
+	private bool m_GameActive = true;
+	public bool GameActive { get { return m_GameActive; } }
+
+	public GameObject AsteroidPrefab;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+			return;
+		}			
+
+		SpawnAsteroids ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnAsteroids() {
+		for (var i = 0; i < 5 + m_Level; ++i) {
+			var asteroid = (GameObject)Instantiate (AsteroidPrefab);
+			var center = Camera.main.rect.center;
+
+			float angle = Random.Range (0.0f, 360.0f);
+			var offset = new Vector2 (Mathf.Sin (angle), Mathf.Cos (angle)) * 5.0f;
+			asteroid.transform.position = center + offset;
+		}
+	}
+}
